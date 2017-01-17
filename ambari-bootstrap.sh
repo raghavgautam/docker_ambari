bd515381c95f97b407a6f757a4f476880236511e
Raghav Kumar Gautam <raghav@apache.org>
2017-01-17 00:43:19 -0800

    better ambar-server status reporting

--- ambari-bootstrap.sh
+++ ambari-bootstrap.sh
@@ -1,9 +1,10 @@
 source common-bootstrap.sh
 
 for i in {1..10}; do
-    ambari-server status && break
+    echo "attemp $i to start ambari"
     ambari-server start
-    echo "try $i failed"
+    ambari-server status && break
     sleep 10
 done
+ambari-server status
 source end-bootstrap.sh
