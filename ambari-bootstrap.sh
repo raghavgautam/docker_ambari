fd8a533f9e2f2b87a6d7199e4f4ff02d23108951
Raghav Kumar Gautam <raghav@apache.org>
2017-01-17 10:57:29 -0800

    check status of ambari server after some time

--- ambari-bootstrap.sh
+++ ambari-bootstrap.sh
@@ -3,8 +3,8 @@ source common-bootstrap.sh
 for i in {1..10}; do
     echo "attemp $i to start ambari"
     ambari-server start
-    ambari-server status && break
     sleep 10
+    ambari-server status && break
 done
 ambari-server status
 source end-bootstrap.sh
